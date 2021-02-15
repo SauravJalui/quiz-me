6031af8b39ec85c3a03293449a83aeba34b74fd3
SauravJalui <saurav.jalui-codes@outlook.com>
2021-02-15 17:15:41 +0530

    added docstrings

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+# using CustonUser model for scalability
 class CustomUser(AbstractUser):
     age = models.PositiveIntegerField(null=True, blank=True)
